519297710f8d904095ebad4cd178b4d44ade3f2f
Svorky <kalex92@yandex.ru>
2024-08-29 16:13:01 +0300

    deploy 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { productRouter } from './routes/productRoute.js';
 import bodyParser from 'body-parser';
 import { languageRouter } from './routes/languageRouter.js';
@@ -8,15 +8,19 @@ import { translationRouter } from './routes/translationRouter.js';
 import { searchRouter } from './routes/searchRoute.js';
 import { pictureRouter } from './routes/pictureRouter.js';
 import userRouter from './routes/userRouter.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({
-  credentials:true,
+  credentials: true,
   // origin: ['http://localhost:5173']
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.json({ limit: '1mb' }));
 
 app.listen(process.env.PORT || 5005, () => {
   console.log(`Server listening on ${process.env.PORT || 5005}`);
@@ -26,11 +30,11 @@ app.listen(process.env.PORT || 5005, () => {
 //   res.json({ message: `Hello ${req.params.name}, from server!` });
 // });
 
-app.use('/api/products', productRouter)
-app.use('/api/languages', languageRouter)
-app.use('/api/translations', translationRouter)
-app.use('/api/picture', pictureRouter)
-app.use('/search', searchRouter)
+app.use('/api/products', productRouter);
+app.use('/api/languages', languageRouter);
+app.use('/api/translations', translationRouter);
+app.use('/api/picture', pictureRouter);
+app.use('/search', searchRouter);
 app.use("/user", userRouter);
 
 // Have Node serve the files for our built React app
