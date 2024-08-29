0d0dfb8768bf9ba7bf615379d0b2907c098b42e5
Svorky <kalex92@yandex.ru>
2024-08-29 16:20:07 +0300

    deploy 4

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ app.use('/search', searchRouter);
 app.use("/user", userRouter);
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 // app.use(express.static(path.join(__dirname, "/client/build")));
 
 // // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 
